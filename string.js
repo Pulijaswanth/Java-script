5a0c8a2f3171d45dc6370e8c6979cce46f6a621d
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-13 13:56:03 +0530

    Add files via upload
    Basic frequently used string operations.

---
+++ string.js
@@ -0,0 +1,23 @@
+let s1="hellow world"
+let s2='hello js'
+let s3=`hello string`//string literal
+
+let name="john"
+let age=20
+console.log(name+" is "+age+" years old");
+console.log(`${name} is ${age} years old`);
+
+let s="javaScript is a scripting language"
+let l=s.length
+console.log(`length of the string is ${1}`);
+console.log(`lenght of the string is ${s.length}`);
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+console.log(s.indexOf('x'));
+console.log(s.lastIndexOf('a'));
+console.log(s.lastIndexOf('j'));
+console.log(s.charAt(11));
+console.log(s.indexOf('scr'));
+console.log(s.replace('a','x'));
+console.log(s.replace('a','xyz'));
+console.log(s.replace('x'));
