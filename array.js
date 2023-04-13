4fd442d2a3abb32cd7c1dec10b23589c32970bbd
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-13 14:35:40 +0530

    Add files via upload
    To demonstrate the declarations and few operations in array.

---
+++ array.js
@@ -0,0 +1,15 @@
+let a=[1,2,3.5,true,"hello"]
+console.log(a);
+a.push(100)
+console.log(a);
+a.pop()
+console.log(a);
+for(let i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+let arr= Array()
+arr.push(10)
+arr.push(20)
+arr.push(30)
+console.log(arr);
