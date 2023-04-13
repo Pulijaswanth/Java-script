222897def0c92b666d589bc1f018b681830d7a4a
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-13 14:19:19 +0530

    Add files via upload
    Some basic math operations in JS.

---
+++ math.js
@@ -0,0 +1,9 @@
+console.log(Math.pow(3,2));
+console.log(Math.sqrt(49));
+console.log(Math.ceil(3.2));//4
+console.log(Math.floor(3.8));//3
+
+let rn=Math.random()
+let nn=rn *100      //if we want only two number multiply it with 100 and if you want 3 number multiply it with 1000 and if you want a 4 digit then multi[ply it with 10000
+console.log(Math.floor(nn));
+console.log(Math.floor(Math.random()*100+1));
