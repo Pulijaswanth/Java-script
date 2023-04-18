a9ffeb474023dcedad908aa1cb33a2fc15ebf849
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-18 11:13:34 +0530

    Add files via upload

---
+++ Areaandcirofcircle.js
@@ -0,0 +1,10 @@
+let r=10
+let pie=3.142
+let area=0
+pie=parseFloat(pie)
+r=parseInt(r)
+area=pie*r*r
+console.log(`area of the circle is ${area}`)
+let circum=0
+circum=2*pie*r
+console.log(`Circumference of the circle is ${circum}`)
