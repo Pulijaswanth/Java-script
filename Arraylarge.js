cba83acef66dcdd6897ce67b8cdf6d1c6e6d423d
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-05-06 10:51:52 +0530

    Add files via upload
    To find the largest element in the array and display that array element in the console.

---
+++ Arraylarge.js
@@ -0,0 +1,11 @@
+let a=[10,20,30,80,50]
+let largest=a[0]
+for(let i=0;i<a.length;i++)
+{
+    if(a[i]>largest)
+    {
+        largest=a[i]
+    }
+}
+console.log("the alrgest element in the array is "+largest)
+
