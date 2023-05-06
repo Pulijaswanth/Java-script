32e639cfc5a8d8b7c7a72f82d72703fb3f4cc331
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-05-06 11:00:43 +0530

    Add files via upload
    To find the larger element from the given three elements.

---
+++ Largeof3.js
@@ -0,0 +1,16 @@
+let a=10
+let b=20
+let c=30
+console.log("The entered numbers are "+a+" "+b+" "+c)
+if(a>b && a>c)
+{
+    console.log(a+" is greater")
+}
+else if (b>a && b>c)
+{
+    console.log(b+" is greater")
+}
+else if(c>a && c>b)
+{
+   console.log(c+" is greater")
+}
