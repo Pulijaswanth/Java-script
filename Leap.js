d7cec59f73ecc6e34903b20ac9ea717a4777ed4f
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-05-06 11:03:19 +0530

    Add files via upload
    To find whether the given year is a leap year or no.

---
+++ Leap.js
@@ -0,0 +1,10 @@
+let year=2016
+console.log("The entered year is "+year)
+if(year%4==0)
+{
+    console.log("The year which is entered is a leap year ")
+}
+else
+{
+   console.log("The year is not a leap year")
+}
